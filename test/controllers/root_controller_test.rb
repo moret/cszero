1cc0bebd47d3b81e28bcff4b3bd5a432c71842db
Danilo Moret <github@moret.pro.br>
2017-08-16 10:29:20 -0300

    Spec root controller

--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    User.collection.drop
+    @user = User.create email: 'user@cszero.com', password: 'secret'
+    Admin.collection.drop
+    @admin = Admin.create email: 'admin@cszero.com', password: 'secret'
+  end
+
+  test "unlogged user or admin get links to sign ins" do
+    get root_path
+    assert_response :success
+    assert_select 'a[href=?]', new_user_session_path, count: 1, text: 'Acesso atendente'
+    assert_select 'a[href=?]', new_admin_session_path, count: 1, text: 'Acesso admin'
+  end
+
+  test "logged user is redirected to cases#index" do
+    sign_in @user
+    get root_path
+    assert_redirected_to cases_path
+  end
+
+  test "logged admin is redirected to users#index" do
+    sign_in @admin
+    get root_path
+    assert_redirected_to users_path
+  end
 end
