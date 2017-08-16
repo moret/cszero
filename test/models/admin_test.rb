f6c00b71815cb7a6e7a5a8ff08f2bcd54dfb6360
Danilo Moret <github@moret.pro.br>
2017-08-16 08:47:59 -0300

    Spec admin and user devise modifications

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Admin.collection.drop
+  end
+
+  test "email must be unique" do
+    firstAdmin = Admin.new email: 'admin@cszero.com', password: 'secret'
+    firstAdmin.save!
+    secondAdmin = Admin.new email: 'admin@cszero.com', password: 'anotherSecret'
+    assert_not secondAdmin.save
+    assert_equal 'is already taken', secondAdmin.errors.messages[:email][0]
+  end
+
+  test "empty will_save_change_to_email? for Rails 5 compatibility" do
+    admin = Admin.new email: 'admin@cszero.com', password: 'secret'
+    assert_nil admin.will_save_change_to_email?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    User.collection.drop
+  end
+
+  test "email must be unique" do
+    firstUser = User.new email: 'user@cszero.com', password: 'secret'
+    firstUser.save!
+    secondUser = User.new email: 'user@cszero.com', password: 'anotherSecret'
+    assert_not secondUser.save
+    assert_equal 'is already taken', secondUser.errors.messages[:email][0]
+  end
+
+  test "empty will_save_change_to_email? for Rails 5 compatibility" do
+    user = User.new email: 'user@cszero.com', password: 'secret'
+    assert_nil user.will_save_change_to_email?
+  end
 end
