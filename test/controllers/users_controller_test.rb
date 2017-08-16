ecc9c8ab3f93b88447a0d8e9664ad12b87e0f3ff
Danilo Moret <github@moret.pro.br>
2017-08-16 11:06:19 -0300

    Spec user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    User.collection.drop
+    Admin.collection.drop
+    @admin = Admin.create email: 'admin@cszero.com', password: 'secret'
+  end
+
+  test "throws out unlogged admins" do
+    get users_path
+    assert_redirected_to new_admin_session_path
+  end
+
+  test "logged in users#index links to create user and sign out" do
+    sign_in @admin
+    get users_path
+    assert_response :success
+    assert_select 'a[href=?]', new_user_path, count: 1, text: 'Criar novo atendente'
+    assert_select 'a[href=?]', destroy_admin_session_path, count: 1, text: 'Sair'
+  end
+
+  test "logged in users#new shows form and link to sign out" do
+    sign_in @admin
+    get new_user_path
+    assert_response :success
+    assert_select 'form', count: 1, action: users_path, method: 'POST'
+    assert_select 'label[for=user_email]', text: 'Email'
+    assert_select 'input#user_email', count: 1, type: 'email', name: 'user[email]'
+    assert_select 'label[for=user_senha]', text: 'Senha'
+    assert_select 'input#user_password', count: 1, type: 'password', name: 'user[password]'
+    assert_select 'a[href=?]', destroy_admin_session_path, count: 1, text: 'Sair'
+  end
+
+  test "logged in users#create creates user and redirects to user#show" do
+    sign_in @admin
+    post users_path, params: { user: { email: 'user@cszero.com', password: 'secret' } }
+    assert_redirected_to user_path(User.find_by email: 'user@cszero.com')
+  end
+
+  test "logged in users#show shows user email and link to sign out" do
+    sign_in @admin
+    get user_path(User.create email: 'user@cszero.com', password: 'secret')
+    assert_response :success
+    assert_select 'span', id: 'email', text: 'user@cszero.com'
+    assert_select 'a[href=?]', destroy_admin_session_path, count: 1, text: 'Sair'
+  end
 end
